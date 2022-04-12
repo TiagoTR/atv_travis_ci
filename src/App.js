5ded9b8335037aaa3f6c0e5db77528b42c72190b
TiagoTR <tiagorocha.tr16@gmail.com>
2022-04-12 01:08:11 -0300

    add travis.yml file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Atividade Travis CI Tiago Rocha
         </p>
         <a
           className="App-link"
